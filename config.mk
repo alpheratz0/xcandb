af19d3b08efa0d0222e19f0ad269978232996761
alpheratz0 <alpheratz99@protonmail.com>
2023-08-28 08:29:33 -0300

    format config.mk

--- config.mk
+++ config.mk
@@ -1,13 +1,11 @@
 # Copyright (C) 2022 <alpheratz99@protonmail.com>
 # This program is free software.
 
-VERSION   = 0.2.0
-
-CC        = cc
-INCS      = -I/usr/X11R6/include -I.
-CFLAGS    = -std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
-LDLIBS    = -lxcb -lxcb-image -lxcb-cursor -lxcb-keysyms -lxcb-icccm -lpng -lxcb-xkb -lsaveas
-LDFLAGS   = -L/usr/X11R6/lib -L./libsaveas -s
-
-PREFIX    = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+VERSION=0.2.0
+CC=cc
+INCS=-I/usr/X11R6/include -I.
+CFLAGS=-std=c99 -pedantic -Wall -Wextra -Os $(INCS) -DVERSION=\"$(VERSION)\"
+LDLIBS=-lxcb -lxcb-image -lxcb-cursor -lxcb-keysyms -lxcb-icccm -lpng -lxcb-xkb -lsaveas
+LDFLAGS=-L/usr/X11R6/lib -L./libsaveas -s
+PREFIX=/usr/local
+MANPREFIX=$(PREFIX)/share/man
